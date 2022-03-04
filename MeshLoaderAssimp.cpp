2a9759fa68ca89ad50a67ae48edfa12fac529e6d
刘东平 <460054202@qq.com>
2022-03-04 15:34:30 +0800

    assimp mesh loader只加载模型，不解析材质，因为大部分情况下这个库拿到的材质都是错的，纹理都拿不全

--- MeshLoaderAssimp.cpp
+++ MeshLoaderAssimp.cpp
@@ -27,18 +27,7 @@ public:
 	void recursive_parse(const struct aiScene* sc, const struct aiNode* nd, std::shared_ptr<Node<glm::mat4>>& pRootNode);
 
 	aiScene* mpAiScene{ nullptr };
-	bool mbMergeAllMesh{ false };
-	vector<float> mPos;//保存顶点坐标
-	vector<float> mNormals;//保存normal
-	vector<float> mTangents;//保存tangents，用于做凹凸贴图
-	vector<float> mTexcoords;//保存纹理坐标值
-	vector<unsigned int> mIndexes;//保存索引值
-	unordered_map<unsigned int, unsigned int> mIndexMap;
-	unsigned int mCurIndex{ 0 };
-	int mMeshNum{ 0 }; //记录mesh的个数
-	std::string mMaterialName;//当前mesh的材质名字
-	std::string mFileName;//当前要加载的文件的名字，不包括文件的路径与后缀名。用于作为material的名字的前缀
-	unordered_map<string, string> mContentNameMap;
+	
 };
 
 
@@ -56,118 +45,15 @@ void MeshLoaderAssimpImpl::recursive_parse(const struct aiScene* sc, const struc
 
 	for (n = 0; n < nd->mNumMeshes; ++n)//遍历本节点下的mesh
 	{
-		std::shared_ptr<Material> pMeshMat;
-		if (!mbMergeAllMesh) {
-			mCurIndex = 0;
-			mPos.clear();
-			mNormals.clear();
-			mTangents.clear();
-			mIndexes.clear();
-			mIndexMap.clear();
-			mTexcoords.clear();
-			mMaterialName.clear();
-		}
-		int indexTranslate = mCurIndex;
+		vector<float> mPos;//保存顶点坐标
+		vector<float> mNormals;//保存normal
+		vector<float> mTexcoords;//保存纹理坐标值
+		vector<unsigned int> mIndexes;//保存索引值
+		unordered_map<unsigned int, unsigned int> mIndexMap;
+
 		const struct aiMesh* mesh = sc->mMeshes[nd->mMeshes[n]];
 		auto pMeshMaterial = sc->mMaterials[mesh->mMaterialIndex];
 		int texCount = 0;
-		//std::string diffuse;
-		//std::string specular;
-		//std::string normal;
-		//std::string mask;
-		//std::map<int, std::string> texMap;
-		//for (int i = 1; i <= 9; ++i) {
-		//	aiString textureFilePath;
-		//	auto texCount = pMeshMaterial->GetTextureCount((aiTextureType)i);
-		//	if (texCount > 0) {
-		//		pMeshMaterial->GetTexture((aiTextureType)i, 0, &textureFilePath);
-		//		texMap.emplace(i, textureFilePath.C_Str());
-		//		std::cout << i << ": " << textureFilePath.C_Str() << std::endl;
-		//		++texCount;
-		//	}
-		//}
-		//std::string matName = mFileName;
-		//std::string program;
-		//std::string sampler;
-		//string op;
-		//if (!texMap.empty()) {
-		//	//1 diff 5 normal 8 mask
-		//	if (texMap.find(1) != texMap.end()) {
-		//		sampler = "\tsampler{\n\t\t";
-		//		sampler += "s_texture=";
-		//		sampler += texMap[1];
-		//		sampler += "\n";
-		//		int normalMapIndex = 0;
-		//		if (texMap.find(5) != texMap.end()) {
-		//			normalMapIndex = 5;
-		//		}
-		//		else if (texMap.find(9) != texMap.end()) {
-		//			normalMapIndex = 9;
-		//		}
-		//		if (normalMapIndex != 0) {
-		//			sampler += "\t\ts_normal=";
-		//			sampler += texMap[normalMapIndex];
-		//			sampler += "\n";
-		//			if (texMap.find(8) != texMap.end()) {
-		//				matName += "_dnlm";//diffuse normal light mask
-		//				program = "\tprogram=posDiffNormalLightMask\n";
-		//				sampler += "\t\tmaskTex=";
-		//				sampler += texMap[8];
-		//				sampler += "\n";
-		//				op = "\top{\n";
-		//				op += "\t\tblend=true,sa,1-sa,add\n";
-		//				op += "\t}\n";
-		//			}
-		//			else {
-		//				matName += "_dnl";
-		//				program = "\tprogram=posDiffNormalLight\n";
-		//			}
-		//		}
-		//		else if (texMap.find(8) != texMap.end()) {
-		//			matName += "_dlm";
-		//			program = "\tprogram=posDiffLightMask\n";
-		//			sampler += "\t\tmaskTex=";
-		//			sampler += texMap[8];
-		//			sampler += "\n";
-		//			op = "\top{\n";
-		//			op += "\t\tblend=true,sa,1-sa,add\n";
-		//			op += "\t}\n";
-		//		}
-		//		else {
-		//			matName += "_dl";
-		//			program = "\tprogram=posDiffLight\n";
-		//		}
-		//		sampler += "\n\t}\n";
-		//	}
-		//	else {
-		//		matName += "_l";
-		//		program = "\tprogram=posLight\n";
-		//	}
-		//}
-		//else {
-		//	//如果没有纹理，这个物体的材质名字就默认为
-		//	matName += "_l";
-		//	program = "\tprogram=posLight\n";
-		//}
-		////写入material信息
-		//stringstream tt;
-		//tt << matName << "_" << mMeshNum;
-		//mMaterialName = tt.str();
-		//string matContents = program + sampler + op;
-		//auto it = mContentNameMap.find(matContents);
-		//if (it != mContentNameMap.end()) {
-		//	mMaterialName = it->second;
-		//	pMeshMat = Material::getMaterial(mMaterialName);
-		//}
-		//else {
-		//	if (Material::parseMaterial(mMaterialName, matContents)) {
-		//		pMeshMat = Material::getMaterial(mMaterialName);
-		//	}
-		//	else {
-		//		LOGE("parse material failed when import mesh");
-		//	}
-		//	mContentNameMap.emplace(matContents, mMaterialName);
-		//}
 		std::unordered_map<unsigned int,unsigned int> indexMap;
 		auto primitivType = mesh->mPrimitiveTypes;
 		if (primitivType == aiPrimitiveType_TRIANGLE) {
@@ -209,68 +95,10 @@ void MeshLoaderAssimpImpl::recursive_parse(const struct aiScene* sc, const struc
 		MeshP pMesh = make_shared<Mesh>(MeshType::MESH_DIY);
 		if (pMesh->loadMesh(mPos, mTexcoords, mNormals, mIndexes)) {
 			pRootNode->addAttachment(pMesh);
-			if (pMeshMat) {
-				pMesh->setMaterial(pMeshMat);
-			}
-			else {
-				LOGE("%s mesh material is null", __func__);
-			}
 		}
 		else {
 			LOGE("%s load mesh failed", __func__);
 		}
-		++mMeshNum;
-
-		//for (t = 0; t < mesh->mNumFaces; ++t) {
-		//	auto face = &mesh->mFaces[t];
-		//	auto drawType = face->mNumIndices;
-		//	if (drawType != 3) {
-		//		std::cout << "discard a mesh,the drawType is <<drawType " << ",not triangle" << std::endl;
-		//		break;
-		//	}
-
-		//	for (int i = 0; i < face->mNumIndices; i++)		// go through all vertices in face
-		//	{
-		//		auto vertexIndex = face->mIndices[i];	// get group index for current index
-		//		auto tempIt = mIndexMap.find(vertexIndex + indexTranslate);
-		//		if (tempIt != mIndexMap.end()) {
-		//			mIndexes.emplace_back(tempIt->second);
-		//		}
-		//		else {
-		//			auto pPos = &mesh->mVertices[vertexIndex];
-		//			auto vec3Size = sizeof(*pPos);
-		//			mPos.emplace_back(pPos->x);
-		//			mPos.emplace_back(pPos->y);
-		//			mPos.emplace_back(pPos->z);
-		//			mIndexes.emplace_back(mCurIndex);
-		//			mIndexMap.emplace(vertexIndex + indexTranslate, mCurIndex);
-		//			++mCurIndex;
-
-		//			if (mesh->mNormals != nullptr)
-		//			{
-		//				auto pNormal = &mesh->mNormals[vertexIndex];
-		//				mNormals.emplace_back(pNormal->x);
-		//				mNormals.emplace_back(pNormal->y);
-		//				mNormals.emplace_back(pNormal->z);
-		//			}
-
-		//			if (mesh->mTangents != nullptr)
-		//			{
-		//				auto pTangent = &mesh->mTangents[vertexIndex];
-		//				mTangents.emplace_back(pTangent->x);
-		//				mTangents.emplace_back(pTangent->y);
-		//				mTangents.emplace_back(pTangent->z);
-		//			}
-
-		//			if (mesh->HasTextureCoords(0))		//HasTextureCoords(texture_coordinates_set)
-		//			{
-		//				mTexcoords.emplace_back(mesh->mTextureCoords[0][vertexIndex].x);
-		//				mTexcoords.emplace_back(mesh->mTextureCoords[0][vertexIndex].y);
-		//			}
-		//		}
-		//	}
-		//}
-		
 	}
 	
 	// recursive all children
@@ -309,7 +137,7 @@ bool MeshLoaderAssimpImpl::loadFromFile(const std::string& filename, std::shared
 		return false;
 	}
 	//拿到mesh的文件名，用于给mesh的材质命名
-	mFileName = Utils::getFileName(filename);
+	//mFileName = Utils::getFileName(filename);
 	
 
 	auto pNode = mpAiScene->mRootNode;
