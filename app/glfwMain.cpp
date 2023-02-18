ef306b04fc201f5fe616fb03ab02d6d47df32385
liudongping <460054202@qq.com>
2023-02-18 02:56:58 -0800

    change opengl to 4.1, because  vmware only support opengl4.1 currently

--- app/glfwMain.cpp
+++ app/glfwMain.cpp
@@ -159,7 +159,7 @@ int main()
 		std::cout<<"glfw init failed\n";
 	}
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
